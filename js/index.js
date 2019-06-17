d4af62019fd5fd6e3c68c29df4d36cb30ac6519e
cmstexas <crystal.m.soria@gmail.com>
2019-06-17 16:50:07 -0500

    close mvp

--- js/index.js
+++ js/index.js
@@ -48,3 +48,100 @@ document.getElementById("logo-img").setAttribute("src", "img/logo.png")
 document.getElementById("cta-img").setAttribute("src", "img/header-img.png")
 
 document.getElementById("middle-img").setAttribute("src", "img/mid-page-accent.jpg")
+
+//NAV SECTION//
+
+const nav = document.querySelector("nav");
+const childNodes = nav.childNodes;
+childNodes[1].innerText = siteContent["nav"]["nav-item-1"];
+childNodes[3].innerText = siteContent["nav"]["nav-item-2"];
+childNodes[5].innerText = siteContent["nav"]["nav-item-3"];
+childNodes[7].innerText = siteContent["nav"]["nav-item-4"];
+childNodes[9].innerText = siteContent["nav"]["nav-item-5"];
+childNodes[11].innerText = siteContent["nav"]["nav-item-6"];
+
+const newItem = document.createElement("a");
+newItem.innerText = 'Member Sign In';
+newItem.href = '#';
+nav.appendChild(newItem);
+
+const newItem2 = document.createElement("a");
+newItem2.innerText = 'Home';
+newItem2.href = '#';
+nav.prepend(newItem2);
+
+
+
+const navItems = document.querySelectorAll("a");
+for (let i = 0; i < navItems.length; i++) {
+navItems[i].style.color = "limegreen";
+}
+
+
+//CTA TEXT SECTION//
+
+const cta = document.querySelector(".cta-text");
+cta.childNodes[1].innerHTML = siteContent["cta"]["h1"]
+cta.childNodes[3].innerText = siteContent["cta"]["button"];
+
+
+//TOP CONTENT SECTION//
+
+const topContent = document.querySelectorAll(".top-content div");
+
+topContent[0].querySelector('h4').textContent = siteContent["main-content"]["features-h4"];
+topContent[0].querySelector('p').textContent = siteContent["main-content"]["features-content"];
+topContent[1].querySelector('h4').textContent = siteContent["main-content"]["about-h4"]
+topContent[1].querySelector('p').textContent = siteContent["main-content"]["about-content"]
+
+
+//MAIN CONTENT SECTION//
+
+const bottomContent = document.querySelectorAll(".bottom-content div");
+
+bottomContent[0].querySelector('h4').textContent = siteContent["main-content"]["services-h4"];
+bottomContent[0].querySelector('p').textContent = siteContent["main-content"]["services-content"];
+bottomContent[1].querySelector('h4').textContent = siteContent["main-content"]["product-h4"];
+bottomContent[1].querySelector('p').textContent = siteContent["main-content"]["product-content"];
+bottomContent[2].querySelector('h4').textContent = siteContent["main-content"]["vision-h4"];
+bottomContent[2].querySelector('p').textContent = siteContent["main-content"]["vision-content"];
+
+const contact = document.querySelector(".contact");
+contact.querySelector("h4").textContent = siteContent["contact"]["contact-h4"];
+contact.querySelectorAll("p")[0].innerHTML = siteContent["contact"]["address"];
+contact.querySelectorAll("p")[1].textContent = siteContent["contact"]["phone"]; 
+contact.querySelectorAll("p")[2].textContent = siteContent["contact"]["email"];
+
+
+
+//CONTACT SECTION//
+
+
+const contactHeader = document.querySelectorAll('.contact h4');
+const contactText = document.querySelectorAll('.contact p');
+
+contactHeader[0].textContent = siteContent.contact['contact-h4'];
+contactText[0].textContent = siteContent.contact.address;
+contactText[1].textContent = siteContent.contact.phone;
+contactText[2].textContent = siteContent.contact.email;
+
+
+//FOOTER//
+
+const footer = document.querySelector('footer');
+footer.childNodes[1].textContent = siteContent.footer.copyright;
+
+
+//STRETCH//
+
+
+//fixed nav//
+
+// const header = document.querySelector("header");
+// header.style.position = "fixed";
+// header.style.top = "0";
+// header.style.zIndex = "1";
+// header.style.backgroundColor = "#fff";
+
+//  cta.style.marginTop = "3rem";
+
